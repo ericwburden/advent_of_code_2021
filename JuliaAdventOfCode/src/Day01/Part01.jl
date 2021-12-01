0a5c2213b173c3f64af094b39f808ec93e795132
Eric Burden <eric@ericburden.dev>
2021-12-01 09:32:51 -0600

    Add useful comments to Day 1

--- JuliaAdventOfCode/src/Day01/Part01.jl
+++ JuliaAdventOfCode/src/Day01/Part01.jl
@@ -1,4 +1,5 @@
 function part1(input)
+    # Returns an array of `1`s and `0`s, where a `1` indicates an increase
     increases(x) = diff(x) .> 0
 
     (input
--- JuliaAdventOfCode/src/Day01/Part02.jl
+++ JuliaAdventOfCode/src/Day01/Part02.jl
@@ -1,5 +1,8 @@
 function part2(input)
+    # Creates an array of views into a sliding window of `x` of length `w`
     windows(x, w) = [view(x, i:i+w) for i in 1:length(x)-w]
+
+    # Returns an array of `1`s and `0`s, where a `1` indicates an increase
     increases(x) = diff(x) .> 0
 
     (windows(input, 3)
