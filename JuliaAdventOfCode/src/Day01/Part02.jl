5ea4ae97fa2651b7684cfbd4ae5939d171cc946d
Eric Burden <eric@ericburden.dev>
2021-12-01 19:54:29 -0600

    Fix off-by-one error

--- JuliaAdventOfCode/src/Day01/Part02.jl
+++ JuliaAdventOfCode/src/Day01/Part02.jl
@@ -1,6 +1,6 @@
 function part2(input)
     # Creates an array of views into a sliding window of `x` of length `w`
-    windows(x, w) = [view(x, i:i+w) for i in 1:length(x)-w]
+    windows(x, w) = [view(x, i:(i+w-1)) for i in 1:(length(x)-w+1)]
 
     # Returns an array of `1`s and `0`s, where a `1` indicates an increase
     increases(x) = diff(x) .> 0
@@ -10,4 +10,3 @@ function part2(input)
     |> increases
     |> sum)
 end
-
