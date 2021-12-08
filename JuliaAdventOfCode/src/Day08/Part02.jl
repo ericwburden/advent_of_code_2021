fddfa2b527cf3507a75d7a49950b85f661c32c6a
Eric Burden <eric@ericburden.dev>
2021-12-08 16:34:27 -0600

    Minor refactor, Day 8 Part 2

--- JuliaAdventOfCode/src/Day08/Part02.jl
+++ JuliaAdventOfCode/src/Day08/Part02.jl
@@ -1,36 +1,3 @@
-function getfivecharmatches(patterns, one, four, seven)::Vector{Tuple{String, Int}}
-    matches = []
-    fournotseven = setdiff(four, seven)
-    istwo(x)   = one ⊈ x  && fournotseven ⊈ x
-    isthree(x) = one ⊆ x
-    isfive(x)  = fournotseven ⊆ x
-    
-    for pattern in patterns
-        match = istwo(pattern)   ? (pattern, 2) :
-                isthree(pattern) ? (pattern, 3) :
-                isfive(pattern)  ? (pattern, 5) : missing
-        ismissing(match) && error("Could not match pattern $pattern")  
-        push!(matches, match)
-    end
-    return matches
-end
-
-function getsixcharmatches(patterns, four, seven)::Vector{Tuple{String, Int}}
-    matches = []
-    iszero(x) = four ⊈ x && seven ⊆ x
-    issix(x)  = four ⊈ x && seven ⊈ x
-    isnine(x) = four ⊆ x && seven ⊆ x
-
-    for pattern in patterns
-        match = iszero(pattern) ? (pattern, 0) :
-                issix(pattern)  ? (pattern, 6) :
-                isnine(pattern) ? (pattern, 9) : missing
-        ismissing(match) && error("Could not match pattern $pattern")
-        push!(matches, match)
-    end
-    return matches
-end
-
 function decode(rawinput::RawInput)::Int
     (patterns, outputs) = rawinput
 
@@ -50,19 +17,53 @@ function decode(rawinput::RawInput)::Int
 
     # Match the 5- and 6-length patterns
     (one, seven, four) = sortedpatterns[1:3]
-    fivecharmatches = getfivecharmatches(sortedpatterns[5:7], one, four, seven)
-    sixcharmatches = getsixcharmatches(sortedpatterns[8:10], four, seven)
+    fivecharmatches = getfivecharmatches(sortedpatterns[5:7], four, seven)
+    sixcharmatches  = getsixcharmatches(sortedpatterns[8:10], four, seven)
     append!(patternmatches, fivecharmatches, sixcharmatches)
 
     # Now with all the patterns deciphered, let's get the 
     # values in our outputs
     patterndict = Dict(patternmatches)
     numbers = reverse([patterndict[output] for output in outputs])
-    return sum(numbers[i] * 10^(i-1) for i in 1:length(numbers))
+    return sum(numbers[i] * 10^(i - 1) for i = 1:length(numbers))
 end
+
+function getfivecharmatches(patterns, four, seven)::Vector{Tuple{String,Int}}
+    matches = []
+    fournotseven = setdiff(four, seven)
+    istwo(x)   = seven ⊈ x && fournotseven ⊈ x
+    isthree(x) = seven ⊆ x
+    isfive(x)  = fournotseven ⊆ x
+
+    for pattern in patterns
+        match = istwo(pattern)   ? (pattern, 2) :
+                isthree(pattern) ? (pattern, 3) :
+                isfive(pattern)  ? (pattern, 5) : missing
+        ismissing(match) && error("Could not match pattern $pattern")
+        push!(matches, match)
+    end
+    return matches
+end
+
+function getsixcharmatches(patterns, four, seven)::Vector{Tuple{String,Int}}
+    matches = []
+    iszero(x) = four ⊈ x && seven ⊆ x
+    issix(x)  = four ⊈ x && seven ⊈ x
+    isnine(x) = four ⊆ x && seven ⊆ x
+
+    for pattern in patterns
+        match = iszero(pattern) ? (pattern, 0) :
+                issix(pattern)  ? (pattern, 6) :
+                isnine(pattern) ? (pattern, 9) : missing
+        ismissing(match) && error("Could not match pattern $pattern")
+        push!(matches, match)
+    end
+    return matches
+end
+
 function part2(input)
     decodeeach(x) = map(decode, x)
     (input
-        |> decodeeach
-        |> sum)
+     |> decodeeach
+     |> sum)
 end
