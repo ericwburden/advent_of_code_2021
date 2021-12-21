e41fccf19431694cfa929eff4858b0280864ccd9
Eric Burden <eric@ericburden.dev>
2021-12-21 08:16:20 -0600

    Squashed some sneaky tabs

--- JuliaAdventOfCode/src/Day18/Ingest.jl
+++ JuliaAdventOfCode/src/Day18/Ingest.jl
@@ -4,15 +4,15 @@
 # Snailfish number. This is the point to split the string when parsing it
 # into a binary tree.
 function middleidx(s::AbstractString)
-	depth = 0
-	for (idx, char) in enumerate(s)
-		if char == '['; depth += 1; end
-		if char == ']'; depth -= 1; end
-		if char == ',' && depth <= 1
-			return idx
-		end
-	end
-	return nothing
+    depth = 0
+    for (idx, char) in enumerate(s)
+        if char == '['; depth += 1; end
+        if char == ']'; depth -= 1; end
+        if char == ',' && depth <= 1
+            return idx
+        end
+    end
+    return nothing
 end
 
 
@@ -28,8 +28,8 @@ const MaybeNode = Union{Nothing,Node}
 
 # The `Leaf` nodes contain the numeric values
 mutable struct Leaf <: Node
-	parent::MaybeNode
-	value::Int64
+    parent::MaybeNode
+    value::Int64
 end
 Leaf(s::AbstractString) = Leaf(nothing, parse(Int, s))
 Leaf(parent::Node, s::AbstractString) = Leaf(parent, parse(Int, s))
@@ -38,34 +38,34 @@ Leaf(parent::Node, s::AbstractString) = Leaf(parent, parse(Int, s))
 # The `Branch` nodes provide the structure, containing a left and right `Node`, 
 # either a `Leaf`,  a `Branch`, or possibly `nothing`
 mutable struct Branch <: Node
-	parent::MaybeNode
-	left::MaybeNode
-	right::MaybeNode
+    parent::MaybeNode
+    left::MaybeNode
+    right::MaybeNode
 end
 Branch(left::Node, right::Node) = Branch(nothing, left, right)
 Branch(::Nothing) = Branch(nothing, nothing, nothing)
 Branch(parent::Node) = Branch(parent, nothing, nothing)
 
 function Branch(s::AbstractString, parent = nothing)
-	all(isnumeric, s) && return Leaf(parent, s)
+    all(isnumeric, s) && return Leaf(parent, s)
 
-	middle = middleidx(s)
-	(leftstr, rightstr) = (s[2:middle-1], s[middle+1:end-1])
-	newbranch = Branch(parent)
-	newbranch.left  = Branch(leftstr,  newbranch)
-	newbranch.right = Branch(rightstr, newbranch)
-	return newbranch
+    middle = middleidx(s)
+    (leftstr, rightstr) = (s[2:middle-1], s[middle+1:end-1])
+    newbranch = Branch(parent)
+    newbranch.left  = Branch(leftstr,  newbranch)
+    newbranch.right = Branch(rightstr, newbranch)
+    return newbranch
 end
 
 
 # A `Tree` is the binary tree, just contains the root `Branch`
 struct Tree
-	root::Node
+    root::Node
 end
 
 function Tree(s::AbstractString)
-	root = Branch(s)
-	return Tree(root)
+    root = Branch(s)
+    return Tree(root)
 end
 
 
--- JuliaAdventOfCode/src/Day18/Part01.jl
+++ JuliaAdventOfCode/src/Day18/Part01.jl
@@ -10,148 +10,148 @@ splitvalue(value) = (splitleft(value), splitright(value))
 # Leaf -------------------------------------------------------------------------
 
 function add!(a::Leaf, b::Leaf) 
-	a.value += b.value
+    a.value += b.value
 end
 function add!(a::Leaf, b::Int64) 
-	a.value += b
+    a.value += b
 end
 
 
 # Node (Leaf/Branch) -----------------------------------------------------------
 
 function traverse(node::Node, depth = 0)
-	if node isa Leaf
-		println("($(node.value), $depth)")
-		return nothing
-	end
-	traverse(node.left, depth + 1)
-	traverse(node.right, depth + 1)
+    if node isa Leaf
+        println("($(node.value), $depth)")
+        return nothing
+    end
+    traverse(node.left, depth + 1)
+    traverse(node.right, depth + 1)
 end
 
 function firstleaf(node::Node)
-	node isa Leaf && return node
-	return firstleaf(node.left)
+    node isa Leaf && return node
+    return firstleaf(node.left)
 end
 
 function lastleaf(node::Node)
-	node isa Leaf && return node
-	return lastleaf(node.right)
+    node isa Leaf && return node
+    return lastleaf(node.right)
 end
 
 function previousleaf(node::Node)
-	# If we hit the root node, there is no previous leaf
-	isnothing(node.parent) && return nothing
+    # If we hit the root node, there is no previous leaf
+    isnothing(node.parent) && return nothing
 
-	if node.parent.left == node
-		# If this is the left node, travel up the tree one level and check for
-		# a previous value starting at the parent node.
-		return previousleaf(node.parent)
-	else
-		# If it's not the left node, then start searching for the rightmost
-		# leaf starting with this node's left sibling.
-		return lastleaf(node.parent.left)
-	end
+    if node.parent.left == node
+        # If this is the left node, travel up the tree one level and check for
+        # a previous value starting at the parent node.
+        return previousleaf(node.parent)
+    else
+        # If it's not the left node, then start searching for the rightmost
+        # leaf starting with this node's left sibling.
+        return lastleaf(node.parent.left)
+    end
 end
 
 function nextleaf(node::Node)
-	# If we hit the root node, there is no previous leaf
-	isnothing(node.parent) && return nothing
+    # If we hit the root node, there is no previous leaf
+    isnothing(node.parent) && return nothing
 
-	if node.parent.right == node
-		# If this is the right node, travel up the tree one level and check for 
-		# a next value starting at the parent node.
-		return nextleaf(node.parent)
-	else
-		# If it's not the right node, then start searching for the leftmost
-		# leaf starting with this node's right sibling
-		return firstleaf(node.parent.right)
-	end
+    if node.parent.right == node
+        # If this is the right node, travel up the tree one level and check for 
+        # a next value starting at the parent node.
+        return nextleaf(node.parent)
+    else
+        # If it's not the right node, then start searching for the leftmost
+        # leaf starting with this node's right sibling
+        return firstleaf(node.parent.right)
+    end
 end
 
 # Find the first node with a depth equal to 4
 function explosivenode(node::Node, depth = 0)
-	node isa Leaf && return nothing
-	depth == 4 && return node
-	left = explosivenode(node.left, depth + 1)
-	isnothing(left) && return explosivenode(node.right, depth + 1)
-	return left
+    node isa Leaf && return nothing
+    depth == 4 && return node
+    left = explosivenode(node.left, depth + 1)
+    isnothing(left) && return explosivenode(node.right, depth + 1)
+    return left
 end
 
 function explode!(node::Node)
-	(parent, left, right) = (node.parent, node.left.value, node.right.value)
+    (parent, left, right) = (node.parent, node.left.value, node.right.value)
 
-	previous = previousleaf(node)
-	if !isnothing(previous)
-		add!(previous, left)
-	end
+    previous = previousleaf(node)
+    if !isnothing(previous)
+        add!(previous, left)
+    end
 
-	next = nextleaf(node)
-	if !isnothing(next)
-		add!(next, right)
-	end
+    next = nextleaf(node)
+    if !isnothing(next)
+        add!(next, right)
+    end
 
-	if parent.left == node
-		parent.left = Leaf(parent, 0)
-	else
-		parent.right = Leaf(parent, 0)
-	end
+    if parent.left == node
+        parent.left = Leaf(parent, 0)
+    else
+        parent.right = Leaf(parent, 0)
+    end
 end
 
 function splitnode(node::Node)
-	if node isa Leaf 
-		node.value > 9 && return node
-		return nothing
-	end
+    if node isa Leaf 
+        node.value > 9 && return node
+        return nothing
+    end
 
 
-	left = splitnode(node.left)
-	left isa Leaf && return left
+    left = splitnode(node.left)
+    left isa Leaf && return left
 
-	right = splitnode(node.right)
-	right isa Leaf && return right
-	
-	return nothing
+    right = splitnode(node.right)
+    right isa Leaf && return right
+    
+    return nothing
 end
 
 function split!(leaf::Leaf)
-	(leftval, rightval) = splitvalue(leaf.value)
-	splitnode = Branch(leaf.parent)
-	splitnode.left  = Leaf(splitnode, leftval)
-	splitnode.right = Leaf(splitnode, rightval)
+    (leftval, rightval) = splitvalue(leaf.value)
+    splitnode = Branch(leaf.parent)
+    splitnode.left  = Leaf(splitnode, leftval)
+    splitnode.right = Leaf(splitnode, rightval)
 
-	if leaf.parent.left == leaf
-		leaf.parent.left = splitnode
-	else
-		leaf.parent.right = splitnode
-	end
+    if leaf.parent.left == leaf
+        leaf.parent.left = splitnode
+    else
+        leaf.parent.right = splitnode
+    end
 end
 
 function magnitude(node::Node)
-	node isa Leaf && return node.value
-	left = magnitude(node.left) * 3
-	right = magnitude(node.right) * 2
-	return left + right
+    node isa Leaf && return node.value
+    left = magnitude(node.left) * 3
+    right = magnitude(node.right) * 2
+    return left + right
 end
 
 
 # Tree -------------------------------------------------------------------------
 
 function combine(a::Tree, b::Tree)
-	root = Branch(nothing)
-	
-	root.left = a.root
-	root.left.parent = root
+    root = Branch(nothing)
+    
+    root.left = a.root
+    root.left.parent = root
 
-	root.right = b.root
-	root.right.parent = root
+    root.right = b.root
+    root.right.parent = root
 
-	return Tree(root)
+    return Tree(root)
 end
 
 function Base.:+(a::Tree, b::Tree)
-	tree = combine(a, b)
-	simplify!(tree)
-	return tree
+    tree = combine(a, b)
+    simplify!(tree)
+    return tree
 end
 
 traverse(tree::Tree) = traverse(tree.root)
@@ -159,25 +159,25 @@ traverse(tree::Tree) = traverse(tree.root)
 # If there is a pair of values nested 4 or more deep, explode that pair and
 # return true. Otherwise, return false.
 function explode!(tree::Tree)
-	explosive = explosivenode(tree.root)
-	isnothing(explosive) && return false
-	explode!(explosive)
-	return true
+    explosive = explosivenode(tree.root)
+    isnothing(explosive) && return false
+    explode!(explosive)
+    return true
 end
 
 function split!(tree::Tree)
-	splitat = splitnode(tree.root)
-	isnothing(splitat) && return false
-	split!(splitat)
-	return true
+    splitat = splitnode(tree.root)
+    isnothing(splitat) && return false
+    split!(splitat)
+    return true
 end
 
 function simplify!(tree::Tree)
-	while true
-		explode!(tree) && continue
-		split!(tree)   && continue
-		break
-	end
+    while true
+        explode!(tree) && continue
+        split!(tree)   && continue
+        break
+    end
 end
 
 magnitude(tree::Tree) = magnitude(tree.root)
